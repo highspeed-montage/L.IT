be98272dfa5c9f26bbe84750dcdbdda9e0f9de8e
82karu1bwi <50798255+82karu1bwi@users.noreply.github.com>
2019-06-01 15:21:34 +0200

    Update Monteur.java
    Hinzufügen einer Deque

--- Montage_Planning/src/models/Monteur.java
+++ Montage_Planning/src/models/Monteur.java
@@ -1,8 +1,11 @@
 package models;
 
+import java.util.LinkedList;
+import java.util.Deque;
+
 public abstract class Monteur extends Mitarbeiter{
 	private boolean verfuegbar;
-	private Rechner rechner;
+	private Deque<Rechner> queue = new LinkedList<Rechner>();
 	
 	public Monteur(int pPersonalnr, String pName, String pVorname, double pUrlaubstage, int pKrankheitstage)
 	{
