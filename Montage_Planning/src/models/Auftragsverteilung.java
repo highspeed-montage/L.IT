e5e8748080f6165de6fcac63322f39b4b87a3fa4
82miga1bwi <82miga1bwi@hft-stuttgart.de>
2019-06-03 00:34:21 +0200

    Klasse Auftragsverteilung erstellt

---
+++ Montage_Planning/src/models/Auftragsverteilung.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.util.Date;
+
+public class Auftragsverteilung {
+	
+	private Date bearbeitungsdatum;
+	private Date lieferdatum;
+	private Integer seriennr;
+	private String status;
+	
+	public Auftragsverteilung(Date bearbeitungsdatum, Date lieferdatum, Integer seriennr, String status) {
+		super();
+		this.bearbeitungsdatum = bearbeitungsdatum;
+		this.lieferdatum = lieferdatum;
+		this.seriennr = seriennr;
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Auftragsverteilung [bearbeitungsdatum=" + bearbeitungsdatum + ", lieferdatum=" + lieferdatum
+				+ ", seriennr=" + seriennr + ", status=" + status + "]";
+	}
+
+	public Date getBearbeitungsdatum() {
+		return bearbeitungsdatum;
+	}
+
+	public void setBearbeitungsdatum(Date bearbeitungsdatum) {
+		this.bearbeitungsdatum = bearbeitungsdatum;
+	}
+
+	public Date getLieferdatum() {
+		return lieferdatum;
+	}
+
+	public void setLieferdatum(Date lieferdatum) {
+		this.lieferdatum = lieferdatum;
+	}
+
+	public Integer getSeriennr() {
+		return seriennr;
+	}
+
+	public void setSeriennr(Integer seriennr) {
+		this.seriennr = seriennr;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}	
+	
+	
+}
