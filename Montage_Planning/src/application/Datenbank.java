88382992413a42c07d9344992671a863f95a0383
82miga1bwi <82miga1bwi@hft-stuttgart.de>
2019-06-03 00:20:31 +0200

    Datenbank
    Abfrage Rechner Liste geändert

--- Montage_Planning/src/application/Datenbank.java
+++ Montage_Planning/src/application/Datenbank.java
@@ -10,11 +10,21 @@ import java.util.List;
 
 import com.mysql.cj.jdbc.result.ResultSetMetaData;
 
+import models.Auftragsverteilung;
+
 public class Datenbank {
 
-	private static final String DB_CONNECTION = "jdbc:mysql://193.196.143.168:3306/aj9s-montage?serverTimezone=UTC";
-	private static final String DB_USER = "aj9s-montage";
-	private static final String DB_PASSWORD = "TPrKrlU9QsMv6Oh7";
+	/*
+	 * private static final String DB_CONNECTION =
+	 * "jdbc:mysql://193.196.143.168:3306/aj9s-montage?serverTimezone=UTC"; private
+	 * static final String DB_USER = "aj9s-montage"; private static final String
+	 * DB_PASSWORD = "TPrKrlU9QsMv6Oh7";
+	 */
+
+	// Datenbankverbindung GABBY LOKAL fürs testen, weil VPN nicht geht
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost:8889/aj9s-montage?serverTimezone=UTC";
+	private static final String DB_USER = "root";
+	private static final String DB_PASSWORD = "root";
 
 	private Connection connection;
 
@@ -60,13 +70,10 @@ public class Datenbank {
 	// Abfrage der Usernamen
 	public ArrayList Usernameabfrage() throws SQLException {
 		Statement stmt = connection.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT idPersonalnummer FROM Mitarbeiter"); // Abfrage wie Personalnummern
-																						// lauten hei�en
-
-		ResultSetMetaData rsmd = (ResultSetMetaData) rs.getMetaData(); // Gr��e der Tabelle
+		ResultSet rs = stmt.executeQuery("SELECT idPersonalnummer FROM Mitarbeiter");
+		ResultSetMetaData rsmd = (ResultSetMetaData) rs.getMetaData();
 		int columnCount = rsmd.getColumnCount(); //
-		ArrayList<String> list = new ArrayList(columnCount); // Erstellt ArrayList
-
+		ArrayList<String> list = new ArrayList(columnCount);
 		while (rs.next()) {
 			int i = 1;
 			while (i <= columnCount) {
@@ -76,39 +83,38 @@ public class Datenbank {
 		return list;
 	}
 
-//	// Abfrage der Passw�rter
-//	public ArrayList Passwortabfrage() throws SQLException {
-//
-//		Statement stmt = connection.createStatement();
-//		ResultSet rs = stmt.executeQuery("SELECT name FROM Mitarbeiter"); // Abfrage wie Mitarbeiter hei�en
-//
-//		ResultSetMetaData rsmd = rs.getMetaData(); // Gr��e der Tabelle
-//		int columnCount = rsmd.getColumnCount(); //
-//		ArrayList<String> list = new ArrayList(columnCount); // Erstellt ArrayList
-//
-//		while (rs.next()) {
-//			int i = 1;
-//			while (i <= columnCount) {
-//				list.add(rs.getString(i++));
-//			}
-//		}
-//		return list;
-//	}
-
+	// // Abfrage der Passw�rter
+	// public ArrayList Passwortabfrage() throws SQLException {
 	//
-	public List<String> listRechnerBySeriennr() throws SQLException {
+	// Statement stmt = connection.createStatement();
+	// ResultSet rs = stmt.executeQuery("SELECT name FROM Mitarbeiter"); // Abfrage
+	// wie Mitarbeiter hei�en
+	//
+	// ResultSetMetaData rsmd = rs.getMetaData(); // Gr��e der Tabelle
+	// int columnCount = rsmd.getColumnCount(); //
+	// ArrayList<String> list = new ArrayList(columnCount); // Erstellt ArrayList
+	//
+	// while (rs.next()) {
+	// int i = 1;
+	// while (i <= columnCount) {
+	// list.add(rs.getString(i++));
+	// }
+	// }
+	// return list;
+	// }
+
+	// Rechner - Listenansicht Daten für Tabelleninhalt
+	public List<Auftragsverteilung> listRechnerAusAuftragsverteilung() throws SQLException {
+		List<Auftragsverteilung> tabelleninhalt = new ArrayList<>();
 		Statement stmt = connection.createStatement();
-		String query = "SELECT Rechner_seriennummer FROM Auftragsverteilung";
+		String query = "SELECT Auftragsverteilung.Datum, Auftragsverteilung.Rechner_seriennummer, "
+				+ "Rechner.Status_idStatus "
+				+ "FROM Auftragsverteilung, Rechner WHERE Auftragsverteilung.Rechner_seriennummer = Rechner.idSeriennummer";
 		ResultSet rs = stmt.executeQuery(query);
-		List<String> rechnerSeriennr = new ArrayList<>();
 		while (rs.next()) {
-			System.out.println(rs.getString("Rechner_seriennummer"));
-			rechnerSeriennr.add(rs.getString("Rechner_seriennummer"));
+			tabelleninhalt.add(new Auftragsverteilung(rs.getDate("Auftragsverteilung.Datum"), rs.getDate("Auftragsverteilung.Datum"),
+					rs.getInt("Auftragsverteilung.Rechner_seriennummer"), rs.getString("Rechner.Status_idStatus")));
 		}
-		return rechnerSeriennr;
+		return tabelleninhalt;
 	}
 }
-	
-	
-	
-	
