b3c6f962d826a7e7b90c2a46bd8ef0ab99bd19ef
82beja1bwi <janbeez99@gmail.com>
2019-06-02 21:47:56 +0200

    Views und Controller FA_Rechnerinfo

---
+++ Montage_Planning/src/controllers/SA_RechnerinfoController.java
@@ -0,0 +1,22 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class SA_RechnerinfoController {
+	/**Rechnerinfo*/
+	@FXML
+	private Label lbl_SAI_status;
+	@FXML
+	private Label lbl_SAI_lieferdatum;
+	@FXML
+	private Label lbl_SAI_bearbeitungsdatum;
+	@FXML
+	private Label lbl_SAI_kunde;
+	@FXML
+	private Label lbl_SAI_kundenNr;
+	@FXML
+	private Label lbl_SAI_kundenEMail;
+	@FXML
+	private Label lbl_SAI_Seriennummer;
+}
