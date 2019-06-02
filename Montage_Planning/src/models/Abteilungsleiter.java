a01015ee016af5ef99bcf63cb77830926703f162
82miga1bwi <82miga1bwi@hft-stuttgart.de>
2019-06-03 01:53:23 +0200

    Vererbung hinzugefügt

--- Montage_Planning/src/models/Abteilungsleiter.java
+++ Montage_Planning/src/models/Abteilungsleiter.java
@@ -1,6 +1,9 @@
 package models;
 
-public class Abteilungsleiter {
-	
+public class Abteilungsleiter extends Mitarbeiter {
+
+	public Abteilungsleiter(int pPersonalnr, String pName, String pVorname, double pUrlaubstage, int pKrankheitstage) {
+		super(pPersonalnr, pName, pVorname, pUrlaubstage, pKrankheitstage);
+	}
 
 }
