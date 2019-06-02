c086068dad9b606584e8328b7a355f52a51485f8
82miga1bwi <82miga1bwi@hft-stuttgart.de>
2019-06-03 00:34:38 +0200

    Vererbung hat gefehlt

--- Montage_Planning/src/models/FA_Rechner.java
+++ Montage_Planning/src/models/FA_Rechner.java
@@ -1,10 +1,14 @@
 package models;
 
-public class FA_Rechner {
+public class FA_Rechner extends Rechner {
+
 	private static final double arbeitsaufwand = 4;
-	
-	public double getArbeitsaufwand()
-	{
+
+	public FA_Rechner(int pSeriennr, Auftrag pAuftrag, String pStatus, Teile pTeile) {
+		super(pSeriennr, pAuftrag, pStatus, pTeile);
+	}
+
+	public double getArbeitsaufwand() {
 		return arbeitsaufwand;
 	}
 }
