94a87f1c379c272c4aa1184130dcfab9e08c9b2e
82beja1bwi <janbeez99@gmail.com>
2019-06-03 12:24:05 +0200

    Datenbank Problem gelöst
    Meine Abfragen von gestern wurden nicht richtig gespeichert, daher wieder eingeügt

--- Montage_Planning/src/application/Datenbank.java
+++ Montage_Planning/src/application/Datenbank.java
@@ -19,7 +19,7 @@ public class Datenbank {
 	private static final String DB_USER = "aj9s-montage";
 	private static final String DB_PASSWORD = "TPrKrlU9QsMv6Oh7";
 
-//	// NICHT LÖSCHEN: Datenbankverbindung GABBY LOKAL fürs testen, weil VPN nicht geht (ich habe mir die Datenbank geklont)
+//	// NICHT LoeSCHEN: Datenbankverbindung GABBY LOKAL fuers testen, weil VPN nicht geht (ich habe mir die Datenbank geklont)
 //	private static final String DB_CONNECTION = "jdbc:mysql://localhost:8889/aj9s-montage?serverTimezone=UTC";
 //	private static final String DB_USER = "root";
 //	private static final String DB_PASSWORD = "root";
@@ -81,14 +81,14 @@ public class Datenbank {
 		return list;
 	}
 
-	// // Abfrage der Passw�rter
+	// // Abfrage der Passwoerter
 	// public ArrayList Passwortabfrage() throws SQLException {
 	//
 	// Statement stmt = connection.createStatement();
 	// ResultSet rs = stmt.executeQuery("SELECT name FROM Mitarbeiter"); // Abfrage
-	// wie Mitarbeiter hei�en
+	// wie Mitarbeiter heissen
 	//
-	// ResultSetMetaData rsmd = rs.getMetaData(); // Gr��e der Tabelle
+	// ResultSetMetaData rsmd = rs.getMetaData(); // Grroesse der Tabelle
 	// int columnCount = rsmd.getColumnCount(); //
 	// ArrayList<String> list = new ArrayList(columnCount); // Erstellt ArrayList
 	//
@@ -101,7 +101,7 @@ public class Datenbank {
 	// return list;
 	// }
 
-	// Rechner - Listenansicht Daten für Tabelleninhalt
+	// Rechner - Listenansicht Daten fuer Tabelleninhalt
 	public List<Auftragsverteilung> listRechnerAusAuftragsverteilung() throws SQLException {
 		List<Auftragsverteilung> tabelleninhalt = new ArrayList<>();
 		Statement stmt = connection.createStatement();
@@ -115,4 +115,97 @@ public class Datenbank {
 		}
 		return tabelleninhalt;
 	}
+	
+	
+	
+	/** Auflisten aller Rechner-Bearbeitungsdaten */
+	public List<Date> listRechnerByBearbeitungsdatum() throws SQLException { // java.util.date
+		Statement stmt = connection.createStatement();
+		String query = "SELECT Datum FROM Auftragsverteilung";
+		ResultSet rs = stmt.executeQuery(query);
+		List<Date> rechnerBearbeitungsdaten = new ArrayList<>(); // Welcher Datentyp?
+
+		while (rs.next()) {
+			java.sql.Date sqlDate = rs.getDate("Datum"); // wie ist der ColumnName wirklich??
+			java.util.Date javaDate = new java.util.Date(sqlDate.getTime()); // Umwandlung von sql.Date zu util.Date
+			rechnerBearbeitungsdaten.add(javaDate);
+		}
+		return rechnerBearbeitungsdaten;
+	}
+	// Hier die Abfragen fuer die Rechnerinformationscontroller
+		/** Seriennummer */
+		//FRAGE: wenn man den Hyperlink Seriennummer  in der Rechner Listenansicht anklickt -> dann muss die Seriennummer irgendwo zwischengespeichert werden!
+		//Dieser Schritt entfaellt deshalb
+		//Die Seriennummer ist fuer viele der folgenden Methoden notwendig
+		
+		
+		
+		
+		
+		/** Kunde */
+		public String getKunde(int serienNummer) throws SQLException {
+			Statement stmt = connection.createStatement();
+			String query = "SELECT Kunde FROM Auftrag WHERE idAuftragsnummer = (SELECT Auftrag_idAuftragsnummer FROM Rechner_Teile WHERE Bezeichnung = '"+serienNummer+"')";
+			ResultSet rs = stmt.executeQuery(query);
+			String kunde = null;
+
+			while (rs.next()) {
+
+				kunde = rs.getString("kunde");
+			}
+			return kunde;
+		}
+		/** KundenEMail */
+		public String getKundenMail(int serienNummer) throws SQLException {
+			//Auftrag holen
+			//Kunde des jwlg Auftrags holen
+			//Kundenid + name auf davon holen
+			Statement stmt = connection.createStatement();
+			String query = "SELECT EMail FROM Kunde WHERE idKundennummer = (SELECT Kunde_idKunde FROM Auftrag WHERE idAuftragsnummer = (SELECT Auftrag_idAuftragsnummer FROM Rechner_Teile WHERE Bezeichnung = '"+serienNummer+"'))";
+			ResultSet rs = stmt.executeQuery(query);
+			String kundenEMail = null;
+
+			while (rs.next()) {
+
+				kundenEMail = rs.getString("EMail");
+			}
+			return kundenEMail;
+		}
+		
+		
+		/** Bearbeitungsdatum */
+		
+		
+		
+		/** Lieferdatum */
+		
+		/** Einzelteile */
+		public List<String> getRechnerEinzelteile(int serienNummer) throws SQLException {
+			Statement stmt = connection.createStatement();
+			String query = "SELECT Bezeichnung FROM Teile WHERE idTeilenummer = (SELECT Teile_idTeilenummer FROM Rechner_Teile WHERE Rechner_idSeriennummer = '"+ serienNummer + "')";
+			ResultSet rs = stmt.executeQuery(query);
+			
+			List<String> rechnerEinzelteile = new ArrayList<>();
+
+			while (rs.next()) {
+				
+				rechnerEinzelteile.add(rs.getString("Bezeichnung"));
+			}
+			return rechnerEinzelteile;
+		}
+
+		/** Einzelteil SUche im Servicauftrag */
+		public int getEinzelteilLagerbestand(String eingabe) throws SQLException {
+			// Hier muss noch gecheckt werden, dass die eingabe ueberhaupt sinnvoll ist.
+			Statement stmt = connection.createStatement();
+			String query = "SELECT Lagerbestand FROM Rechner_Teile WHERE Bezeichnung = '" + eingabe + "'";
+			ResultSet rs = stmt.executeQuery(query);
+			int lagerbestand=0;
+
+			while (rs.next()) {
+
+				lagerbestand = rs.getInt("Lagerbestand");
+			}
+			return lagerbestand;
+		}
 }
