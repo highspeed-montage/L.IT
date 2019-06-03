85a9295af904c760190caa78bec621f85773a9a1
82miga1bwi <82miga1bwi@hft-stuttgart.de>
2019-06-03 21:53:08 +0200

    Rechner-Wochenansicht bearbeitet

--- Montage_Planning/src/controllers/RechneransichtController.java
+++ Montage_Planning/src/controllers/RechneransichtController.java
@@ -2,14 +2,11 @@ package controllers;
 
 import java.net.URL;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
 import java.util.ResourceBundle;
-
 import application.Datenbank;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -24,8 +21,6 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import models.Auftragsverteilung;
-import models.Mitarbeiter;
-import models.Rechner;
 
 public class RechneransichtController implements Initializable {
 
@@ -41,12 +36,12 @@ public class RechneransichtController implements Initializable {
 
 	// Listenansicht
 	@FXML
-	private ComboBox comboBox_RL_filter;
+	private ComboBox<String> comboBox_RL_filter;
 	@FXML
 	private TextField txt_RL_sucheingabe;
 	@FXML
 	private TableView<Auftragsverteilung> tableRechnerListe;
-	
+
 	@FXML
 	private TableColumn<Auftragsverteilung, Integer> col_RL_serienNr;
 	@FXML
@@ -58,63 +53,105 @@ public class RechneransichtController implements Initializable {
 
 	// Wochenansicht
 	@FXML
-	private ComboBox comboBox_RW_Wochenansicht;
+	private ComboBox<String> comboBox_RW_Wochenansicht;
 	@FXML
-	private TableColumn col_RW_Montag;
+	private TableView<Auftragsverteilung> tableRechnerWoche;
 	@FXML
-	private TableColumn col_RW_Dienstag;
+	private TableColumn<Auftragsverteilung, Integer> col_RW_Montag;
 	@FXML
-	private TableColumn col_RW_Mittwoch;
+	private TableColumn<Auftragsverteilung, Integer> col_RW_Dienstag;
 	@FXML
-	private TableColumn col_RW_Donnerstag;
+	private TableColumn<Auftragsverteilung, Integer> col_RW_Mittwoch;
 	@FXML
-	private TableColumn col_RW_Freitag;
+	private TableColumn<Auftragsverteilung, Integer> col_RW_Donnerstag;
+	@FXML
+	private TableColumn<Auftragsverteilung, Integer> col_RW_Freitag;
+
+	Datenbank db = new Datenbank();
+
+	// Woche1 03.06.2019-07.06.2019
+	LocalDate woche1_tag1 = LocalDate.of(2019, Month.JUNE, 03);
+	LocalDate woche1_tag2 = LocalDate.of(2019, Month.JUNE, 04);
+	LocalDate woche1_tag3 = LocalDate.of(2019, Month.JUNE, 05);
+	LocalDate woche1_tag4 = LocalDate.of(2019, Month.JUNE, 06);
+	LocalDate woche1_tag5 = LocalDate.of(2019, Month.JUNE, 07);
+
+	// Woche2 10.06.2019-14.06.2019
+	LocalDate woche2_tag1 = LocalDate.of(2019, Month.JUNE, 10);
+	LocalDate woche2_tag2 = LocalDate.of(2019, Month.JUNE, 11);
+	LocalDate woche2_tag3 = LocalDate.of(2019, Month.JUNE, 12);
+	LocalDate woche2_tag4 = LocalDate.of(2019, Month.JUNE, 13);
+	LocalDate woche2_tag5 = LocalDate.of(2019, Month.JUNE, 14);
+
+	// Woche3 17.06.2019-21.06.2019
+	LocalDate woche3_tag1 = LocalDate.of(2019, Month.JUNE, 17);
+	LocalDate woche3_tag2 = LocalDate.of(2019, Month.JUNE, 18);
+	LocalDate woche3_tag3 = LocalDate.of(2019, Month.JUNE, 19);
+	LocalDate woche3_tag4 = LocalDate.of(2019, Month.JUNE, 20);
+	LocalDate woche3_tag5 = LocalDate.of(2019, Month.JUNE, 21);
 
 	// Funktionen: Wochenansicht:
 	// Table: Auftragsverteilung
 	// dropDown Wochenauswahl
-	// f�r jeden MA werden alle seine Auftr�ge f�r die jwlg. Woche angezeigt
-	// Klick auf �berschrift einer Tabellenspalte (col_RW_Montag...)
-	// Inhalt zu Tabelle hinzuf�gen (MA und Rechner)
-	// REchner m�ssen verschoben werden, wenn sie das t�glich maximale Arbeitspensum
+	// fuer jeden MA werden alle seine Auftraege fuer die jwlg. Woche angezeigt
+	// Inhalt zu Tabelle hinzufuegen (MA und Rechner)
+	// REchner muessen verschoben werden, wenn sie das taeglich maximale
+	// Arbeitspensum
 	// des MAs (4h/8h)
 
-	// Wenn Benutzer-Rolle = Monteur, dann Laden aller Rechner als Liste
-
-	
-
-	
-	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		// Rechner - Wochenansicht Tabelle befüllen
-		
-		// Combo-Box mit Wochen befüllen
-		
-		Date ersteWoche = new Date();
-		Date zweiteWoche = new Date();
-		Date dritteWoche = new Date();
-		
-		ersteWoche = printSimpleDateFormat();
-		
-		ObservableList<Date> options = FXCollections.observableArrayList(
-			        ersteWoche,
-			        zweiteWoche,
-			        dritteWoche			        
-			    );
-		
-		comboBox_RW_Wochenansicht = new ComboBox(options);		
-		
-		// Rechner - Listenansicht Tabelle befüllen
-		ObservableList<Auftragsverteilung> rechnerListenansichtTabelle = FXCollections.observableArrayList();
-		Datenbank db = new Datenbank();
+
 		db.openConnection();
 
+		// ComboBox Wochenangaben als String speichern
+		DateTimeFormatter f = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+		String woche1 = woche1_tag1.format(f) + "-" + woche1_tag5.format(f);
+		String woche2 = woche2_tag1.format(f) + "-" + woche2_tag5.format(f);
+		String woche3 = woche3_tag1.format(f) + "-" + woche3_tag5.format(f);
+
+		// ComboBox Wochenangaben Liste
+		ObservableList<String> options = FXCollections.observableArrayList(woche1, woche2, woche3);
+
+		// ComboBox befüllen
+		comboBox_RW_Wochenansicht.setItems(options);
+
+		// Wochen- und Listenansicht Inhalte
+		wochenansichtFuellen();
+		listenansichtFuellen();
+
+		// db.closeConnection();
+	}
+
+	// Wochenansicht Tabelle befüllen
+	public void wochenansichtFuellen() {
+
+		ObservableList<Auftragsverteilung> rechnerWochenansichtTabelle = FXCollections.observableArrayList();
+
 		try {
-			// User fehlt noch
-			rechnerListenansichtTabelle.addAll(db.listRechnerAusAuftragsverteilung());
-			System.out.println(rechnerListenansichtTabelle.toString());
+			rechnerWochenansichtTabelle.addAll(db.listRechnerAusAuftragsverteilungWoche(/* LoginController.user */));
+			System.out.println(rechnerWochenansichtTabelle.toString());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		col_RW_Montag.setCellValueFactory(new PropertyValueFactory<>("seriennr"));
+		// col_RW_Dienstag.setCellValueFactory(new PropertyValueFactory<>("seriennr"));
+		// col_RW_Mittwoch.setCellValueFactory(new PropertyValueFactory<>("seriennr"));
+		// col_RW_Donnerstag.setCellValueFactory(new
+		// PropertyValueFactory<>("seriennr"));
+		// col_RW_Freitag.setCellValueFactory(new PropertyValueFactory<>("seriennr"));
+
+		tableRechnerWoche.setItems(rechnerWochenansichtTabelle);
+	}
+
+	// Listenansicht Tabelle befüllen
+	public void listenansichtFuellen() {
+
+		ObservableList<Auftragsverteilung> rechnerListenansichtTabelle = FXCollections.observableArrayList();
+
+		try {
+			rechnerListenansichtTabelle.addAll(db.listRechnerAusAuftragsverteilungListe(/* LoginController.user */));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -123,17 +160,8 @@ public class RechneransichtController implements Initializable {
 		col_RL_status.setCellValueFactory(new PropertyValueFactory<>("status"));
 		col_RL_bearbeitungsdatum.setCellValueFactory(new PropertyValueFactory<>("bearbeitungsdatum"));
 		col_RL_lieferdatum.setCellValueFactory(new PropertyValueFactory<>("lieferdatum"));
-		
+
 		tableRechnerListe.setItems(rechnerListenansichtTabelle);
-		
-		
 	}
 
-
-    static Date printSimpleDateFormat() { 
-        SimpleDateFormat formatter = new SimpleDateFormat( 
-                "dd.MM.yyyy"); 
-        Date currentTime = new Date(); 
-        return currentTime;
-    } 
 }
