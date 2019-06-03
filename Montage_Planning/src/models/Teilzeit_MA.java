3a0c71e20e5d5d72f15509355ec3b46d1b758c30
82miga1bwi <82miga1bwi@hft-stuttgart.de>
2019-06-03 16:15:51 +0200

    Konstante groß geschrieben

--- Montage_Planning/src/models/Teilzeit_MA.java
+++ Montage_Planning/src/models/Teilzeit_MA.java
@@ -1,24 +1,24 @@
 package models;
 
-public class Teilzeit_MA extends Monteur{
-	private static final int arbeitszeit = 4;
+public class Teilzeit_MA extends Monteur {
+	private static final int ARBEITSZEIT = 4;
 	private boolean vormittags;
-	
-	public Teilzeit_MA(int pPersonalnr, String pName, String pVorname, double pUrlaubstage, int pKrankheitstage, boolean pVormittags)
-	{
+
+	public Teilzeit_MA(int pPersonalnr, String pName, String pVorname, double pUrlaubstage, int pKrankheitstage,
+			boolean pVormittags) {
 		super(pPersonalnr, pName, pVorname, pUrlaubstage, pKrankheitstage);
 		vormittags = pVormittags;
 	}
-	public int getArbeitszeit()
-	{
-		return arbeitszeit;
+
+	public int getArbeitszeit() {
+		return ARBEITSZEIT;
 	}
-	public void setVormittags(boolean pVormittags)
-	{
+
+	public void setVormittags(boolean pVormittags) {
 		vormittags = pVormittags;
 	}
-	public boolean getVormittags()
-	{
+
+	public boolean getVormittags() {
 		return vormittags;
 	}
 }
