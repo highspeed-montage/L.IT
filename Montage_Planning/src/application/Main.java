638a02a2feff2d95ea047e909ef4cf0857427bea
82beja1bwi <janbeez99@gmail.com>
2019-06-07 15:06:10 +0200

    test main

--- Montage_Planning/src/application/Main.java
+++ Montage_Planning/src/application/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/views/FA_Rechnerinfo.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/views/Rechneranss.fxml"));
 			Scene scene = new Scene(root, 800, 800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
