9f1e852f92f768a6555a7a1205b1e355bfb79332
82beja1bwi <janbeez99@gmail.com>
2019-06-09 18:49:37 +0200

    SA_Info Toggle Group

--- Montage_Planning/src/controllers/SA_RechnerinfoController.java
+++ Montage_Planning/src/controllers/SA_RechnerinfoController.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.Datenbank;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,6 +19,8 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -38,6 +42,8 @@ public class SA_RechnerinfoController implements EventHandler, Initializable {
 	private Label lbl_SAI_Seriennummer;
 
 	/** Problemdokumentation */
+	@FXML
+	private ToggleGroup toggleDokumentation;
 	@FXML
 	private ComboBox<String> comboBox_SAI_Bearbeitungsstatus;
 	@FXML
@@ -108,26 +114,42 @@ public class SA_RechnerinfoController implements EventHandler, Initializable {
 				
 			}
 		});
+		/**Radio button toggle Group Doku
+		 * Problem muss in Rechner gepackt werden
+		 * -> fehlt noch.. WO KOMMT DAS HIN?
+		 * => SA Konstruktor ändern
+		 * */
+		toggleDokumentation.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Toggle> ov, Toggle toggle, Toggle new_toggle) {
+				RadioButton rb = (RadioButton)toggle.getToggleGroup().getSelectedToggle();
+				System.out.println("(test)Selected rb: " + rb.getText());
+				String statusNeu = rb.getText();
+				//ab in die DB damit
+			}
+		
+		});
 		
 
 	}
 
-	/**
-	 * Reagiert auf RadioButton Doku des Problems
-	 * (Hardware/Software/Kundenverschulet) schreib Problem in DB
-	 */
-	// WO WIRD DAS ALLES IN DB GESCHRIEBEN???
-	// Entsprechende DB Methode muss erstellt werden
-	@FXML
-	public void dokuRdbtn(ActionEvent event) {
-		if (rbtn_SAI_Hardware.isSelected()) {
-
-		} else if (rbtn_SAI_Software.isSelected()) {
-
-		} else if (rbtn_SAI_Kunde.isSelected()) {
-
-		}
-	}
+//	/**
+//	 * Reagiert auf RadioButton Doku des Problems
+//	 * (Hardware/Software/Kundenverschulet) schreib Problem in DB
+//	 */
+//	// WO WIRD DAS ALLES IN DB GESCHRIEBEN???
+//	// Entsprechende DB Methode muss erstellt werden
+//	@FXML
+//	public void dokuRdbtn(ActionEvent event) {
+//		if (rbtn_SAI_Hardware.isSelected()) {
+//
+//		} else if (rbtn_SAI_Software.isSelected()) {
+//
+//		} else if (rbtn_SAI_Kunde.isSelected()) {
+//
+//		}
+//	}
 
 	/**
 	 * Bekommt gewähltes Element der ComboBox Aktualisiert dementsprechend den
