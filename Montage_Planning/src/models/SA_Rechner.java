1baf82709e9a86a7ea35aae2f1b25b664714975e
82beja1bwi <janbeez99@gmail.com>
2019-06-11 22:21:22 +0200

    SA_Rechner Konstruktor,getter,setter für DB Klasse

--- Montage_Planning/src/models/SA_Rechner.java
+++ Montage_Planning/src/models/SA_Rechner.java
@@ -4,93 +4,76 @@ import java.util.Date;
 import java.util.List;
 
 public class SA_Rechner extends Rechner {
-	
-	private double arbeitsaufwand;	//Braucht jemand diese Variable?
-	private boolean getestet;
+
+//	private double arbeitsaufwand;	
+//	private boolean getestet;
+//	private boolean neues_Betriebssystem;
+
+	private static final double ARBEITSAUFWAND_SA = 2;
+	// Boolean zu Problemfällen (für rbtns der Doku)
 	private boolean kundenverschuldet;
+	private boolean hardwareverschuldet;
+	private boolean softwareverschuldet;
+	// Boolean zu HW-Schäden (rbtns unter hw-rbtn)
 	private boolean prozessor_kaputt;
 	private boolean grafikkarte_kaputt;
 	private boolean festplatte_kaputt;
 	private boolean dvd_Laufwerk_kaputt;
-	private boolean neues_Betriebssystem;
 
-	public SA_Rechner(int pSeriennr, Auftrag pAuftrag, String pStatus, List<Teile> pTeile, double pArbeitsaufwand) {
+	public SA_Rechner(int pSeriennr, Auftrag pAuftrag, String pStatus, List<Teile> pTeile) {
 		super(pSeriennr, pAuftrag, pStatus, pTeile);
-		this.arbeitsaufwand = pArbeitsaufwand;
-	}
-//	// Konstruktor fÃ¼r DB Abfrage getSARechnerInfo()
-//			public SA_Rechner(int pSeriennr, int auftragsNr, String pStatus, Date pBearbeitungsdatum, Date pLieferdatum,
-//					String pFirmenname, String pPrivatname, int pKundenId,String pEMail, Teile pTeile) {
-//				super(pSeriennr, auftragsNr, pStatus, pBearbeitungsdatum, pLieferdatum, pFirmenname, pPrivatname, pKundenId, pEMail, pTeil);
-//			
-//			}
-			//Vorschläge:
-			//1.)Teile pTeile, String pProblem (hw/sw/kv),
-			//2.)private static final double ARBEITSAUFWAND = 4;
-			//   public double getArbeitsaufwand() {return ARBEITSAUFWAND;}
-			//3.)ET Suche in SA_RC -> 
-
-	public void setArbeitsaufwand(double pArbeitsaufwand) {
-		arbeitsaufwand = pArbeitsaufwand;
 	}
 
-	public void setGetestet(boolean pGetestet) {
-		getestet = pGetestet;
+	/** Konstruktor für DB-Interaktionen */
+	public SA_Rechner(int pSeriennr, int auftragsNr, String pStatus, Date pBearbeitungsdatum, Date pLieferdatum,
+			String pFirmenname, String pPrivatname, int pKundenId, String pEMail, boolean pKundenverschuldet,
+			boolean pHardwareverschuldet, boolean pSoftwareverschuldet, boolean pProzessor_kaputt,
+			boolean pGrafikkarte_kaputt, boolean pFestplatte_kaputt, boolean pDvd_Laufwerk_kaputt) {
+		super(pSeriennr, auftragsNr, pStatus, pBearbeitungsdatum, pLieferdatum, pFirmenname, pPrivatname, pKundenId,
+				pEMail);
+		this.kundenverschuldet = pKundenverschuldet;
+		this.hardwareverschuldet = pHardwareverschuldet;
+		this.softwareverschuldet = pSoftwareverschuldet;
+		this.prozessor_kaputt = pProzessor_kaputt;
+		this.grafikkarte_kaputt = pGrafikkarte_kaputt;
+		this.festplatte_kaputt = pFestplatte_kaputt;
+		this.dvd_Laufwerk_kaputt = pDvd_Laufwerk_kaputt;
 	}
 
 	public void setKundenverschuldet(boolean pKundenverschuldet) {
-		kundenverschuldet = pKundenverschuldet;
+		this.kundenverschuldet = pKundenverschuldet;
 	}
 
 	public void setProzessor_kaputt(boolean pProzessor) {
-		prozessor_kaputt = pProzessor;
+		this.prozessor_kaputt = pProzessor;
 	}
 
 	public void setGrafikkarte_kaputt(boolean pGrafikkarte) {
-		grafikkarte_kaputt = pGrafikkarte;
+		this.grafikkarte_kaputt = pGrafikkarte;
 	}
 
 	public void setFestplatte_kaputt(boolean pFestplatte) {
-		festplatte_kaputt = pFestplatte;
+		this.festplatte_kaputt = pFestplatte;
 	}
 
 	public void setDvd_Laufwerk_kaputt(boolean pLaufwerk) {
-		dvd_Laufwerk_kaputt = pLaufwerk;
+		this.dvd_Laufwerk_kaputt = pLaufwerk;
 	}
 
-	public void setNeues_Betriebssystem(boolean pBetriebssystem) {
-		neues_Betriebssystem = pBetriebssystem;
+	public void setKundenverschuldet1(boolean pKunde) {
+		this.kundenverschuldet = pKunde;
 	}
 
-	public double getArbeitsaufwand() {
-		return arbeitsaufwand;
+	public void setSoftwareverschuldet(boolean pSoftware) {
+		this.softwareverschuldet = pSoftware;
 	}
 
-	public boolean getGetestet() {
-		return getestet;
+	public void setHardwareverschuldet(boolean pHardware) {
+		this.hardwareverschuldet = pHardware;
 	}
 
-	public boolean getKundenverschuldet() {
-		return kundenverschuldet;
+	public static double getArbeitsaufwandSa() {
+		return ARBEITSAUFWAND_SA;
 	}
 
-	public boolean getProzessor() {
-		return prozessor_kaputt;
-	}
-
-	public boolean getGrafikkarte() {
-		return grafikkarte_kaputt;
-	}
-
-	public boolean getFestplatte() {
-		return festplatte_kaputt;
-	}
-
-	public boolean getLaufwerk() {
-		return dvd_Laufwerk_kaputt;
-	}
-
-	public boolean getBetriebssystem() {
-		return neues_Betriebssystem;
-	}
 }
