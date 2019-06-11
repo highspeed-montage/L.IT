ea8fdde3c2e64bbb58166c0460403b9254d0b830
82beja1bwi <janbeez99@gmail.com>
2019-06-11 16:27:14 +0200

    Revert "Datenbankabfragen"
    This reverts commit 80f95b341db70610aefcf14e990340133730dbd5.
    
    # Conflicts:
    #	Montage_Planning/src/application/Datenbank.java
    #	Montage_Planning/src/models/Teile.java

--- Montage_Planning/src/models/Teile.java
+++ Montage_Planning/src/models/Teile.java
@@ -2,29 +2,28 @@ package models;
 
 public class Teile {
 	private int teilenr;
-	private String bezeichnung;
 	private String kategorie;
 	private int lagerbestand;
+	private String bezeichnung;
 	
-	public Teile(int pTeilenr, String pBezeichnung, String pKategorie, int pLagerbestand)
+	public Teile(int pTeilenr, String pKategorie, int pLagerbestand)
 	{
 		teilenr = pTeilenr;
 		kategorie = pKategorie; 
 		lagerbestand = pLagerbestand;
-		bezeichnung = pBezeichnung;
 	}
 	//Konstruktor fuer Db Abfrage getFARechnerInfo()
 	public Teile(String bezeichnung) {
 		this.bezeichnung = bezeichnung;
 	}
+	
 	public void setKategorie(String pKategorie)
 	{
 		kategorie = pKategorie;
 	}
-	public void setBezeichung(String pBezeichnung)
-	{
-		bezeichnung = pBezeichnung;
-	}
+	
+
+	//Lagerbestand berechnen?
 	public int getTeilenr()
 	{
 		return teilenr;
@@ -37,12 +36,19 @@ public class Teile {
 	{
 		return lagerbestand;
 	}
-	public String getBezeichnung()
-	{
+
+	public String getBezeichnung() {
 		return bezeichnung;
 	}
+	@Override
 	public String toString() {
 		return "Teile [teilenr=" + teilenr + ", kategorie=" + kategorie + ", lagerbestand=" + lagerbestand
 				+ ", bezeichnung=" + bezeichnung + "]";
 	}
+
+	
+
+	
+
+	
 }
